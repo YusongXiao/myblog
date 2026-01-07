94883815d5a3648589ce1f7d35ef57b034c3d62c
SongHappy <official@songhappy.cn>
2026-01-07 21:44:30 +0800

    good

--- src/config.ts
+++ src/config.ts
@@ -43,12 +43,12 @@ export const navBarConfig: NavBarConfig = {
 	links: [
 		LinkPreset.Home,
 		LinkPreset.Archive,
+		LinkPreset.About,
 		{
 			name: "Server",
 			url: "https://dash.sh0.cn/", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
-		LinkPreset.About,
 		{
 			name: "GitHub",
 			url: "https://github.com/YusongXiao/myblog", // Internal links should not include the base path, as it is automatically added
